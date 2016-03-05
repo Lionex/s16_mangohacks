7af3b2b805f48a30c0113853c0151aa51b8f5b02
Gwen Lofman <gwen@lofman.co>
2016-03-05 07:38:54 -0500

    Roughed out blog functionality.

--- JEGL/Handler/PostNew.hs
+++ JEGL/Handler/PostNew.hs
@@ -3,20 +3,25 @@ module Handler.PostNew where
 import Import
 import Yesod.Form.Bootstrap3
 
-data BlogPost = BlogPost
-        { title :: Text
-        , article :: Text
-        }
 
 --areq returns an AForm and takes three arguments
 
 blogPostForm :: AForm Handler BlogPost
 blogPostForm = BlogPost
             <$> areq textField (bfs ("Title" :: Text)) Nothing
-            <*> areq textField (bfs ("Blog Post" :: Text)) Nothing
+            <*> areq textField (bfs ("Article" :: Text)) Nothing
 
 getPostNewR :: Handler Html
 getPostNewR = do
     (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm blogPostForm
     defaultLayout $ do
         $(widgetFile "posts/new")
+
+postPostNewR :: Handler Html
+postPostNewR = do
+    ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm blogPostForm
+    case res of
+        FormSuccess blogPost -> do
+            blogPostId <- runDB $ insert blogPost
+            redirect $ PostDetailsR blogPostId
+        _ -> defaultLayout $(widgetFile "posts/new")
